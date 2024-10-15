fa840b009fee539a8e2adf3cdbfdab3dbdf9a1e3
Basavaraj Patil <95280149+basvarajpatil@users.noreply.github.com>
2024-10-15 17:12:30 +0530

    Create ai_enabled_chat.sh

---
+++ ai_enabled_chat.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#This script activates the conda environment and runs the Python script
+
+#Navigate to the project directory
+cd /home/ubuntu/AI-Enabled-Chat-Bot || exit
+
+#Activate the conda environment
+source /home/ubuntu/anaconda3/bin/activate ai_enabled_chat
+
+#Run the Python script
+python work_with_class.py
