6d2cd30e389a32f269d9e78639288baecd0b2e0d
Basavaraj Patil <95280149+basvarajpatil@users.noreply.github.com>
2024-12-20 12:24:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ if __name__ == "__main__":
     from waitress import serve
     warnings.filterwarnings("ignore")
     try:
-        serve(app, host="0.0.0.0", port=5001, threaded=True)
+        serve(app, host="0.0.0.0", port=5001)
     except Exception as e:
         print(f"Error starting server: {e}")
 
